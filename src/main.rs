adce4d53ef877a43517b87efaedc9cf6643b7861
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2026-06-12 16:53:26 +0530

    First image with blended colors and ray math

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ use std::env;
 use std::fs::File;
 use std::io::{BufWriter,Write};
 use OxidisedRayTracing::color::{print_color, Color};
+use OxidisedRayTracing::ray::Ray;
+use OxidisedRayTracing::vec3::{Point3, Vec3};
+
+fn ray_color(ray:&Ray)->Color{
+    let unit_direction = ray.direction().unit_vector();
+    let a = 0.5*(unit_direction.y()+1.0);
+    (1.0-a)*Color::new(1f64,1f64,1f64)+a*Color::new(0.5,0.7,1f64)
+}
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -13,17 +21,38 @@ fn main() -> std::io::Result<()> {
     let file = File::create(filename)?;
     let mut writer = BufWriter::new(file);
 
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let mut image_height = (image_width as f64 / aspect_ratio) as u32;
+    if image_height < 1 {
+        image_height = 1;
+    }
+
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (image_width as f64/image_height as f64);
+
+    let focal_length = 1.0;
+    let camera_center = Point3::new(0.0, 0.0, 0.0);
+
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    let pixel_data_u = viewport_u/image_width as f64;
+    let pixel_data_v = viewport_v/image_height as f64;
+
+    let viewport_upper_left = camera_center - Vec3::new(0.0,0.0,focal_length) - viewport_u/2.0 - viewport_v/2.0;
+    let pixel00_loc = viewport_upper_left + 0.5* (pixel_data_u + pixel_data_v);
+
 
     writeln!(writer,"P3\n{} {}\n255", image_width, image_height)?;
     for j in 0..image_height {
         eprintln!("Scanlines remaining: {}", image_height - j);
         for i in 0..image_width {
-            let r = i as f64 / (image_width - 1) as f64;
-            let g = j as f64 / (image_height - 1) as f64;
-            let b = 0f64;
-            print_color(&Color::new(r, g, b), &mut writer);
+            let pixel_center = pixel00_loc + (i as f64*pixel_data_u)+(j as f64*pixel_data_v);
+            let ray_direction = pixel_center - camera_center;
+            let r = Ray::new(camera_center, ray_direction);
+            let pixel_color = ray_color(&r);
+            print_color(&pixel_color, &mut writer);
         }
     }
     writer.flush()?;
