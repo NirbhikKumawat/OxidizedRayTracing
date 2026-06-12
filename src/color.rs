409c37e8e699c1302b3f3876991847e1faf950af
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2026-06-12 16:24:45 +0530

    Added writing image directly file without shell redirection for making things fast

---
+++ src/color.rs
@@ -0,0 +1,15 @@
+use std::io::Write;
+use crate::vec3::Vec3;
+
+pub type Color = Vec3;
+pub fn print_color(color: &Color,buffer:&mut impl Write) {
+    let r = color.x();
+    let g = color.y();
+    let b = color.z();
+
+    let ir = (255.999 * r) as i32;
+    let ig = (255.999 * g) as i32;
+    let ib = (255.999 * b) as i32;
+
+    writeln!(buffer, "{} {} {}", ir, ig, ib).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
-fn main() {
+use std::env;
+use std::fs::File;
+use std::io::{BufWriter,Write};
+use OxidisedRayTracing::color::{print_color, Color};
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <filename>", args[0]);
+        std::process::exit(1);
+    }
+    let filename = &args[1];
+    let file = File::create(filename)?;
+    let mut writer = BufWriter::new(file);
+
     let image_width = 256;
     let image_height = 256;
 
-    println!("P3\n{} {}\n255", image_width, image_height);
-
+    writeln!(writer,"P3\n{} {}\n255", image_width, image_height)?;
     for j in 0..image_height {
         eprintln!("Scanlines remaining: {}", image_height - j);
         for i in 0..image_width {
             let r = i as f64 / (image_width - 1) as f64;
             let g = j as f64 / (image_height - 1) as f64;
             let b = 0f64;
-
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-            println!("{} {} {} \n", ir, ig, ib);
+            print_color(&Color::new(r, g, b), &mut writer);
         }
     }
+    writer.flush()?;
+    Ok(())
 }
