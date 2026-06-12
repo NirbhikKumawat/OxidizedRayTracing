decdb54dbd4286b7974d180ea33637507ab20e25
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2026-06-12 16:28:53 +0530

    Added ray structure

---
+++ src/ray.rs
@@ -0,0 +1,20 @@
+use crate::vec3::{Point3, Vec3};
+
+struct Ray {
+    origin: Point3,
+    direction: Vec3,
+}
+impl Ray {
+    pub fn new(origin: Point3, direction: Vec3) -> Self{
+        Self {origin, direction}
+    }
+    pub fn origin(&self) -> Point3 {
+        self.origin
+    }
+    pub fn direction(&self) -> Vec3 {
+        self.direction
+    }
+    pub fn point_at(&self, t: f64) -> Point3 {
+        self.origin + self.direction * t
+    }
+}
