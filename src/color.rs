b83c51a565baf1ae0f03c25f4f10aad5aba2c266
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2026-06-14 12:51:04 +0530

    Applied gamma correction

--- src/color.rs
+++ src/color.rs
@@ -8,6 +8,10 @@ pub fn print_color(color: &Color, buffer: &mut impl Write) {
     let g = color.y();
     let b = color.z();
 
+    let r = linear_to_gamma(r);
+    let g = linear_to_gamma(g);
+    let b = linear_to_gamma(b);
+
     let intensity = Interval::new(0.0, 0.999);
 
     let ir = (255.999 * intensity.clamp(r)) as i32;
@@ -15,4 +19,11 @@ pub fn print_color(color: &Color, buffer: &mut impl Write) {
     let ib = (255.999 * intensity.clamp(b)) as i32;
 
     writeln!(buffer, "{} {} {}", ir, ig, ib).unwrap();
+}
+#[inline]
+fn linear_to_gamma(linear_component: f64) -> f64 {
+    if linear_component > 0.0 {
+        return linear_component.sqrt();
+    }
+    0f64
 }
