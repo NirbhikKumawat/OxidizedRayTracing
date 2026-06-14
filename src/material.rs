7100c467a7a60cc67f12476de0a175596a81fc5f
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2026-06-14 14:15:09 +0530

    Added fuzz for metal class

--- src/material.rs
+++ src/material.rs
@@ -36,15 +36,20 @@ impl Material for Lambertian {
 }
 pub struct Metal {
     pub albedo: Color,
+    pub fuzz: f64,
 }
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Self { albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Self {
+        Self {
+            albedo,
+            fuzz: if fuzz < 1.0 { fuzz } else { 1.0 },
+        }
     }
 }
 impl Material for Metal {
     fn scatter(&self, rin: &Ray, record: &HitRecord) -> Option<ScatteredRecord> {
         let reflected = rin.direction().reflect(record.normal);
+        let reflected = reflected.unit_vector() + (self.fuzz * Vec3::random_unit_vector());
         let scattered = Ray::new(record.p, reflected);
         let attenuation = self.albedo;
         Some(ScatteredRecord {
