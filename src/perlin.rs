d68133ceb0a79014d74f90e248ce29036b0ed157
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2026-06-18 16:22:28 +0530

    Added perlin noise with trilinear interpolation

--- src/perlin.rs
+++ src/perlin.rs
@@ -1,5 +1,5 @@
 use crate::utility::{random_f64, random_int};
-use crate::vec3::Point3;
+use crate::vec3::{Point3};
 
 pub struct Perlin<const N: usize> {
     rand_float: [f64; N],
@@ -24,20 +24,26 @@ impl<const N: usize> Perlin<N> {
         }
     }
     pub fn noise(&self, p: &Point3) -> f64 {
-        let i = (4.0 * p.x()).floor() as i32;
-        let j = (4.0 * p.y()).floor() as i32;
-        let k = (4.0 * p.z()).floor() as i32;
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
 
-        let mask = (N - 1) as i32;
-        let i = (i & mask) as usize;
-        let j = (j & mask) as usize;
-        let k = (k & mask) as usize;
+        let i = p.x().floor() as i32;
+        let j = p.y().floor() as i32;
+        let k = p.z().floor() as i32;
 
-        let index_x = self.perm_x[i];
-        let index_y = self.perm_y[(index_x + j) & (N - 1)];
-        let index_z = self.perm_z[(index_y + k) & (N - 1)];
-
-        self.rand_float[index_z]
+        let mut c = [[[0.0; 2]; 2]; 2];
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    let x = ((i + di as i32) & 255) as usize;
+                    let y = ((j + dj as i32) & 255) as usize;
+                    let z = ((k + dk as i32) & 255) as usize;
+                    c[di][dj][dk] = self.rand_float[x^y^z];
+                }
+            }
+        }
+        Self::trilinear_interp(c,u,v,w)
     }
     fn perlin_generate_perm() -> [usize; N] {
         let mut p = [0;N];
@@ -49,5 +55,16 @@ impl<const N: usize> Perlin<N> {
             p.swap(i, target);
         }
         p
+    }
+    fn trilinear_interp(c:[[[f64;2];2];2],u:f64,v:f64,w:f64) -> f64 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum+=(i as f64*u+(1.0-i as f64)*(1.0-u)) * (j as f64*v+(1.0-j as f64)*(1.0-v)) * (k as f64*w+(1.0-k as f64)*(1.0-w)) * c[i][j][k];
+                }
+            }
+        }
+        accum
     }
 }
